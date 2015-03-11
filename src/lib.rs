87c3c3e6bb5984b44fbd104b1fdc57748ffc7c79
zahari <zarchaoz@gmail.com>
2015-03-11 18:47:45 +0200

    Using the libc::memcpy function instead of a custom one.

--- src/lib.rs
+++ src/lib.rs
@@ -6,25 +6,20 @@ mod ffi;
 
 use std::ffi::CString;
 
-use ffi::nginx::{ngx_str_t, ngx_http_request_t, ngx_pool_t, size_t};
+use ffi::nginx::{ngx_str_t, ngx_http_request_t, ngx_pool_t};
+
+use libc::{c_void, size_t};
 
 
-#[no_mangle]
-pub unsafe extern fn memcpy(dest: *mut u8, src: *const u8,
-                            n: usize) -> *mut u8 {
-   let mut i = 0;
-   while i < n {
-      *dest.offset(i as isize) = *src.offset(i as isize);
-      i += 1;
-   }
-   return dest;
+extern {
+   pub fn memcpy(dest: *mut c_void, src: *const c_void, n: size_t) -> *mut c_void;
 }
 
 
 #[no_mangle]
 pub extern fn sample_text_from_rust(
    r: *const ngx_http_request_t,
-   ngx_pcalloc: extern fn(pool: *mut ngx_pool_t, size: size_t) -> *mut ::libc::c_void
+   ngx_pcalloc: extern fn(pool: *mut ngx_pool_t, size: size_t) -> *mut c_void
 ) -> ngx_str_t
 {
    let s = CString::new("hellohihi").unwrap();
@@ -35,7 +30,7 @@ pub extern fn sample_text_from_rust(
    };
 
    unsafe {
-      memcpy(result.data, s.as_ptr() as *const u8, 9);
+      memcpy(result.data as *mut c_void, s.as_ptr() as *const c_void, 9);
    }
 
    result
