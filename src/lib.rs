33e7dcf6733f37351cd51acf81e2be950d6c69b2
zahari <zarchaoz@gmail.com>
2015-03-18 17:48:26 +0200

    Moving the http handler to the Rust code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(libc)]
 #![feature(plugin)]
+#![feature(alloc)]
 
 #![plugin(maud_macros)]
 
@@ -16,11 +17,12 @@ use std::ffi::CString;
 use std::ptr;
 use std::ptr::PtrExt;
 use std::mem;
+use std::boxed;
 
 use ffi::nginx::{
    ngx_str_t, ngx_http_request_t, ngx_pcalloc, ngx_palloc, ngx_conf_t, ngx_command_t, ngx_http_core_module,
    ngx_http_conf_ctx_t, ngx_int_t, ngx_http_output_filter, ngx_chain_t, ngx_http_send_header, ngx_buf_t,
-   ngx_uint_t, ngx_http_core_loc_conf_t, ngx_conf_log_error,
+   ngx_uint_t, ngx_http_core_loc_conf_t, ngx_log_error_core,
 };
 
 use libc::{size_t, c_void, c_uchar, c_char};
@@ -63,13 +65,13 @@ macro_rules! ngx_http_conf_get_module_loc_conf {
 
 macro_rules! ngx_alloc_buf {
    ($pool:expr) => (
-      ngx_palloc($pool, mem::size_of::<ngx_buf_t>())
+      ngx_palloc($pool, mem::size_of::<ngx_buf_t>() as u64)
    )
 }
 
 macro_rules! ngx_calloc_buf {
    ($pool:expr) => (
-      ngx_pcalloc($pool, mem::size_of::<ngx_buf_t>())
+      ngx_pcalloc($pool, mem::size_of::<ngx_buf_t>() as u64)
    )
 }
 
@@ -85,35 +87,71 @@ pub extern fn ngx_http_sample_module_command(
 
    unsafe {
       let clcf: *mut ngx_http_core_loc_conf_t = (*((*((*cf).ctx as *mut ngx_http_conf_ctx_t)).loc_conf).offset(ngx_http_core_module.ctx_index as isize)) as *mut ngx_http_core_loc_conf_t;
-      (*clcf).handler = Some(ngx_http_sample_handler_wrapper);
-
-      let s = CString::new("DUMP  name: %V").unwrap();
-
-      ngx_conf_log_error(
-         1, cf, 0,
-         s.as_ptr(),
-         &(*clcf).name);
+      (*clcf).handler = Some(ngx_http_sample_handler);
    }
 
    //return NGX_CONF_OK as *mut c_char;
    NGX_CONF_OK as *mut c_char
 }
 
-extern {
-   fn ngx_http_sample_handler(r: *mut ngx_http_request_t) -> ngx_int_t;
-}
-
 
 #[no_mangle]
-pub extern fn ngx_http_sample_handler_wrapper(r: *mut ngx_http_request_t) -> ngx_int_t
+pub extern fn ngx_http_sample_handler(r: *mut ngx_http_request_t) -> ngx_int_t
 {
    unsafe {
-      ngx_http_sample_handler(r)
+      let log = (*(*r).connection).log;
+
+      let s = CString::new("DUMP  method: %d").unwrap();
+
+      ngx_log_error_core(
+         2, log, 0,
+         s.as_ptr(),
+         (*r).method
+      );
+
+      401 as i64
+
+      let ngx_http_sample_text: ngx_str_t = sample_text_from_rust(r);
+
+      (*r).headers_out.status = NGX_HTTP_OK;
+      (*r).headers_out.content_length_n = ngx_http_sample_text.len as i64;
+
+      let rc: ngx_int_t = ngx_http_send_header(r);
+//      if rc == NGX_ERROR || rc > NGX_OK { //|| (*r).header_only) {
+//         return rc;
+//      }
+/*
+      let b: *mut ngx_buf_t = ngx_calloc_buf!((*r).pool) as *mut ngx_buf_t;
+
+      if b.is_null() {
+         return NGX_HTTP_INTERNAL_SERVER_ERROR as i64;
+      };
+
+      let out: Box<ngx_chain_t> = Box::new(
+         ngx_chain_t {
+            buf: b,
+            next: ptr::null_mut(),
+         }
+      );
+
+      let out: *mut ngx_chain_t = boxed::into_raw(out);
+
+      (*b).start = ngx_http_sample_text.data;
+      (*b).pos = (*b).start;
+
+      (*b).end = ngx_http_sample_text.data.offset(ngx_http_sample_text.len as isize );
+      (*b).last = (*b).end;
+   //   (*b).last_buf = 1;
+   //   (*b).memory = 1;
+   //   (*b).last_in_chain = 1;
+
+      ngx_http_output_filter(r, out)
+   */
    }
 }
 
-#[no_mangle]
-pub extern fn sample_text_from_rust(r: *const ngx_http_request_t) -> ngx_str_t
+
+fn sample_text_from_rust(r: *mut ngx_http_request_t) -> ngx_str_t
 {
    let name = "Nginx-Rust";
    let markup = html! {
