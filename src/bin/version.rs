0a03ae09b2a5cee5cf5d816d30dd6698fb592156
majorz <zahari@polydraw.com>
2015-03-28 23:13:30 +0200

    Bumping the Nginx version to 1.7.11.

--- src/bin/version.rs
+++ src/bin/version.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-const DEFAULT_NGINX_VERSION: (u16, u16, u16) = (1, 7, 10);
+const DEFAULT_NGINX_VERSION: (u16, u16, u16) = (1, 7, 11);
 
 
 pub fn nginx_version_string() -> String {
